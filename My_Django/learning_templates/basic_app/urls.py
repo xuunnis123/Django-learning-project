af6ed5c92835288d122b9613103379fe28024f15
Ji-Yang Lin <xuunnis123@gmail.com>
2021-03-16 16:07:01 +0800

    Add login, registeration.html
    Adjust some code

--- My_Django/learning_templates/basic_app/urls.py
+++ My_Django/learning_templates/basic_app/urls.py
@@ -8,5 +8,5 @@ app_name='basic_app'
 urlpatterns=[
     path('relative/',views.relative,name='relative'),
     path('other/',views.other,name='other'),
-
+    path('register/',views.register,name='register'),
 ]
